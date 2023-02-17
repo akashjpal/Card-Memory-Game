472b2228fb9edf984a0fc3f228007284b3488702
Akash Pal <91068643+akashjpal@users.noreply.github.com>
2023-02-18 01:35:28 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -5,12 +5,35 @@
 //     items.classList.remove("tog");
 //   })
 // })
-document.querySelectorAll(".tog")[1].classList.remove("tog");
-const images = document.querySelectorAll(".tog");
+// document.querySelectorAll(".tog")[1].classList.remove("tog");
+// const images = document.querySelectorAll(".tog");
+
+
 
   // Loop over each image and add an event listener to remove the class on click
-  images.forEach(function(image) {
-    image.addEventListener("click", function() {
-      image.classList.remove("tog");
-    });
+  for(var i=0;i<document.querySelectorAll(".card").length;i++){
+    document.querySelectorAll(".card")[i].addEventListener("click", function() {
+      // alert("Clicked!!");
+      // alert(i);
+      // alert(this.id);
+      document.querySelector("."+this.id).classList.remove("tog");
+      // alert("Clicked!!");
+    // document.querySelector("image"+(i%6)).classList.remove(".tog");
   });
+}
+// }
+
+// function flip(i){
+//   alert("reached"+i);
+//   document.querySelectorAll(".car-img-top")[i].classList.remove("tog");
+// }
+  // document.addEventListener("click",function(){
+  //   const val = document.querySelectorAll(".tog").value;
+  // alert(val);
+  // images.forEach(function(image) {
+  //   image.addEventListener("click", function() {
+  //     image.classList.remove("tog");
+  //   });
+  // });
+  // })
+  
